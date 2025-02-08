b124f447b03cd024e0924b9d52bd63b794f5bd74
Jo Hooton <40861027+jo-hooton@users.noreply.github.com>
2025-02-08 10:36:31 +0000

    add cart reset

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,19 +5,23 @@ class CartsController < ApplicationController
     @cart_items = @cart.cart_items.order(:id).includes(:product)
   end
 
+  def reset
+    @cart.cart_items.destroy_all
+
+    respond_to do |format|
+      format.html { redirect_to cart_path }
+      format.turbo_stream
+    end
+  end
+
   private
 
-   # def set_cart
-   #   @cart = Cart.find_by(id: session[:cart_id])
+  def set_cart
+    @cart = Cart.find_by(id: session[:cart_id])
 
-   #   if @cart.nil?
-   #     @cart = Cart.create
-   #     session[:cart_id] = @cart.id  # Store cart_id in session
-   #   end
-   # end
-   #
-   def set_cart
-    @cart = Cart.find_by(id: session[:cart_id]) || Cart.create
-    session[:cart_id] ||= @cart.id
+    if @cart.nil?
+      @cart = Cart.create
+      session[:cart_id] = @cart.id  # Store cart_id in session
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   root 'products#index'
 
   resources :products, only: [:index]
-  resources :carts, only: [:show] do
+  resources :carts, only: [:show, :destroy] do
     resources :cart_items, only: [:create, :update, :destroy], shallow: true
+    delete :reset, on: :collection
   end
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
